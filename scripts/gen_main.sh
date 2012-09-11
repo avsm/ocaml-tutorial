e4aea4eec9e9dce39c94248aa8e34a97d0ddacfc
Anil Madhavapeddy <anil@recoil.org>
2012-09-11 11:23:22 +0100

    add build scaffolding for ocaml-tutorial, based on mirage-tutorial

---
+++ scripts/gen_main.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -e
+
+mir-crunch -name "static" static > src/filesystem_static.ml
+echo open Filesystem_static > src/main.ml
+echo open Server >> src/main.ml
+echo "let _ = OS.Main.run (main ())" >> src/main.ml
