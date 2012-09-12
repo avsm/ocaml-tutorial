5ce6ece0c55645253c82b9d20ba1b8529bc0b577
Anil Madhavapeddy <anil@recoil.org>
2012-09-12 19:22:21 +0100

    refresh the Lwt tutorial for UNIX instead of Mirage

--- src/lwt_tutorial.ml
+++ src/lwt_tutorial.ml
@@ -31,18 +31,18 @@ let slides = [
   content= <:html<
     <h3>Lightweight Threads</h3>
     <ul>
-     <li>Lwt provides threads that are <b>event-driven</b> with no preemption. An Lwt program executes until
-         it cannot make progress, and sleeps until woken up by an I/O event or timeout.</li>
+     <li>Lwt provides threads that are <b>event-driven</b> with no preemption.</li>
+     <li> An Lwt-driven program 
+         executes all threads sequentially until none can make progress, and then sleeps until woken up by an I/O event or timeout.</li>
      <li><tt>Lwt</tt> abstracts event callbacks to maintain the illusion of straight-line control flow in code you write.</li>
     </ul>
     <p>Let's look at some examples.
     They are all in<br /> <a href="https://github.com/avsm/ocaml-tutorial/tree/master/">ocaml-tutorial/examples/lwt</a>, and you build them by:</p>
     <section><pre class="noprettyprint">
+$str:dl$ ocaml setup.ml -configure
 $str:dl$ ocaml setup.ml -build
-$str:dl$ ./_build/unix-socket/sleep.bin</pre></section> 
-    <p>More tutorial content is available at:<br /><a href="http://www.openmirage.org/wiki/tutorial-lwt">http://openmirage.org/wiki/tutorial-lwt</a> <i>(this tutorial)</i><br /><a href="http://ocsigen.org/lwt/manual/">http://ocsigen.org/lwt/manual/</a> <i>(Lwt manual)</i></p>
+$str:dl$ ./sleep.byte</pre></section> 
 >>
-  
 }; 
 { 
   styles=[];
@@ -68,20 +68,20 @@ val join : unit Lwt.t list -> unit Lwt.t
     <h3>A Simple Sleeping Example</h3>
 <pre>
   Lwt.bind
-    (OS.Time.sleep 1.0)
+    (Lwt_unix.sleep 1.0)
     (fun () ->
        Lwt.bind
-         (OS.Time.sleep 2.0)
+         (Lwt_unix.sleep 2.0)
          (fun () ->
-            OS.Console.log "Wake up sleepy!\n";
+            print_endline "Wake up sleepy!\n";
             Lwt.return ()
          )
     )</pre>
 <p>More natural ML style, via syntax extension:</p>
 <pre>
-  lwt () = OS.Time.sleep 1.0 in
-  lwt () = OS.Time.sleep 2.0 in
-  OS.Console.log "Wake up sleepy!\n";
+  lwt () = Lwt_unix.sleep 1.0 in
+  lwt () = Lwt_unix.sleep 2.0 in
+  print_endline "Wake up sleepy!\n";
   Lwt.return ()</pre>
   >>
 };
@@ -89,19 +89,19 @@ val join : unit Lwt.t list -> unit Lwt.t
   styles=[];
   content= <:html<
    <h3>Lwt syntax extension</h3>
-<p><tt>lwt/sleep.ml</tt> (<tt>make sleep</tt>)</p>
+<p><tt>lwt/sleep.ml</tt></p>
 <pre>
-  lwt () = OS.Time.sleep 1.0 in
-  lwt () = OS.Time.sleep 2.0 in
-  OS.Console.log "Wake up sleepy!\n";
+  lwt () = Lwt_unix.sleep 1.0 in
+  lwt () = Lwt_unix.sleep 2.0 in
+  print_endline "Wake up sleepy!\n";
   Lwt.return ()</pre>
-<p>After syntax transform: (<tt>make sleep.pp</tt>)</p>
+<p>After syntax transform:</p>
 <pre>
-  let __pa_lwt_0 = OS.Time.sleep 1.0 in
+  let __pa_lwt_0 = Lwt_unix.sleep 1.0 in
   Lwt.bind __pa_lwt_0 (fun () ->
-    let __pa_lwt_0 = OS.Time.sleep 2.0 in
+    let __pa_lwt_0 = Lwt_unix.sleep 2.0 in
     Lwt.bind __pa_lwt_0 (fun () -> 
-      (OS.Console.log "Wake up sleepy!\n";
+      (print_endline "Wake up sleepy!\n";
        Lwt.return ()
       )
     )
@@ -112,19 +112,15 @@ val join : unit Lwt.t list -> unit Lwt.t
   styles=[];
   content= <:html<
     <h3>Behind the Scenes</h3>
-    <p>The scheduler is itself written in OCaml, but is operating system specific. To consider UNIX:</p>
-    <ul>
-      <li>Scheduler: <tt><a href="http://github.com/avsm/mirage/blob/master/lib/os/unix/main.ml">mirage/lib/os/unix/main.ml</a></tt></li>
-      <li>OCaml: <tt><a href="http://github.com/avsm/mirage/blob/master/lib/os/runtime_unix/evtchn_stubs.c">mirage/lib/os/unix/evtchn_stubs.c</a></tt></li>
-    </ul>
+    <p>Many Lwt core modules are portable, such as <tt>Lwt</tt>, <tt>Lwt_stream</tt> and <tt>Lwt_mvar</tt>. The thread scheduler is itself written in OCaml, and is operating system specific.  On Unix, this is <tt>Lwt_unix</tt> for most POSIX calls, and <tt>Lwt_main</tt> to execute the main loop.</p>
+    <p>Lwt uses <tt>libev</tt> under the hood, which waits for I/O events using the best available interface (select, kqueue or epoll).
+    Timeouts are stored in an efficient priority queue, and so millions of threads can be sleeping simultaneously, limited only by heap memory.</p>
     <pre>
 let t,u = Lwt.task () in // t sleeps forever
 Lwt.wakeup u "foo";      // and u can wake it up  
 t                        // value carried by t is "foo" </pre>
 <p>The outside world wakes up sleeping threads via the <tt><a href="http://ocsigen.org/lwt/api/Lwt#VALwakeup">Lwt.wakeup</a></tt> mechanism:</p>
 <ul>
-<li>Timeouts are stored in an efficient <a href="https://github.com/avsm/mirage/blob/master/lib/os/unix/time.ml">priority queue</a></li>
-<li>I/O is woken up by <tt>select</tt>, <tt>kqueue</tt> or <tt>epoll</tt></li>
 </ul>
   >>
 };
