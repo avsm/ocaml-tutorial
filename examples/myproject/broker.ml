f49d52048d8a88b62a3c25d6f782b6a5195c0b40
Anil Madhavapeddy <anil@recoil.org>
2012-09-13 08:23:19 +0200

    add example proj

---
+++ examples/myproject/broker.ml
@@ -0,0 +1,51 @@
+open Core.Std
+open Async.Std
+open Protocol
+
+(* CR yminsky: consider adding an RPC that blocks on the server side. *)
+let publish_impl dir msg =
+  Directory.publish dir msg;
+  return ()
+
+let subscribe_impl dir topic ~aborted =
+  return (
+    match Directory.subscribe dir topic with
+    | None -> Error ()
+    | Some pipe ->
+      whenever (aborted >>| fun () -> Pipe.close_read pipe);
+      Ok pipe
+  )
+;;
+
+let dump_impl dir () =
+  return (Directory.dump dir)
+
+let implementations =
+  [ Rpc.Rpc.     implement publish_rpc   publish_impl
+  ; Rpc.Pipe_rpc.implement subscribe_rpc subscribe_impl
+  ; Rpc.Rpc.     implement dump_rpc      dump_impl
+  ]
+
+let start_server () =
+  let server =
+    match
+      Rpc.Server.create
+        ~implementations
+        ~on_unknown_rpc:`Ignore
+    with
+    | Ok x -> x
+    | Error (`Duplicate_implementations _) -> assert false
+  in
+  let directory = Directory.create () in
+  Tcp.serve ~port:8080 ~on_handler_error:`Ignore
+    (fun _addr r w ->
+      Rpc.Connection.server_with_close r w
+        ~connection_state:directory
+        ~on_handshake_error:`Ignore
+        ~server
+    )
+
+let () =
+  whenever (start_server ());
+  never_returns (Scheduler.go ())
+
---
+++ examples/myproject/protocol.ml
@@ -0,0 +1,41 @@
+open Core.Std
+open Async.Std
+
+module Username : Identifier = String
+module Topic    : Identifier = String
+
+module Message = struct
+  type t = { text: string;
+             topic: Topic.t;
+             from: Username.t;
+             time: Time.t;
+           }
+  with sexp, bin_io
+end
+
+let publish_rpc = Rpc.Rpc.create
+  ~name:"publish"
+  ~version:0
+  ~bin_query:Message.bin_t
+  ~bin_response:Unit.bin_t
+
+let subscribe_rpc = Rpc.Pipe_rpc.create
+  ~name:"subscribe"
+  ~version:0
+  ~bin_query:Topic.bin_t
+  ~bin_response:Message.bin_t
+  ~bin_error:Unit.bin_t
+
+module Dump = struct
+  type single = { topic : Topic.t;
+                  message : Message.t;
+                  num_subscribers: int; }
+  with sexp,bin_io
+  type t = single list with sexp,bin_io
+end
+
+let dump_rpc = Rpc.Rpc.create
+  ~name:"dump"
+  ~version:0
+  ~bin_query:Unit.bin_t
+  ~bin_response:Dump.bin_t
