52dc5329d4084c8018650ad34cb5162561179e90
Anil Madhavapeddy <anil@recoil.org>
2012-09-13 08:08:17 +0200

    fix requirements

--- src/intro.ml
+++ src/intro.ml
@@ -39,96 +39,16 @@ let slides = [
 {
   styles=[];
   content= <:html<
-    <h3>Schedule</h3>
-    <table>
-      <tr style="background-color:#000; color:#EEE">
-         <th>Topic</th>
-         <th>Activity</th>
-         <th>Time</th>
-      </tr>
-      <tr style=$str:pres$>
-         <td>What is Mirage?</td>
-         <td>Presentation</td>
-         <td>10 mins</td>
-      </tr>
-      <tr style=$str:activity$>
-         <td>Hello World in UNIX, Xen, Javascript</td>
-         <td>Activity</td>
-         <td>15 mins</td>
-      </tr>
-      <tr style=$str:pres$>
-         <td>Threading Intro</td>
-         <td>Presentation</td>
-         <td>10 mins</td>
-      </tr>
-      <tr style=$str:activity$>
-         <td>Threading Exercises</td>
-         <td>Activity</td>
-         <td>15 mins</td>
-      </tr>
-      <tr style=$str:rest$>
-         <td colspan="2">Comfort Break</td>
-         <td>10 mins</td>
-      </tr>
-      <tr style=$str:pres$>
-         <td>Device Model and Storage</td>
-         <td>Presentation</td>
-         <td>10 mins</td>
-      </tr>
-      <tr style=$str:activity$>
-         <td>File System Exercises</td>
-         <td>Activity</td>
-         <td>15 mins</td>
-      </tr>
-      <tr style=$str:rest$>
-         <td colspan="2">Tea Break</td>
-         <td>30 mins</td>
-      </tr>
-      <tr style=$str:pres$>
-         <td>Networking</td>
-         <td>Presentation</td>
-         <td>10 mins</td>
-      </tr>
-      <tr style=$str:pres$>
-         <td>Syntax Extensions</td>
-         <td>Presentation</td>
-         <td>5 mins</td>
-      </tr>
-      <tr style=$str:activity$>
-         <td>Build Your Website</td>
-         <td>Activity</td>
-         <td>15 mins</td>
-      </tr>
-      <tr style=$str:pres$>
-         <td>To the Cloud!</td>
-         <td>Presentation</td>
-         <td>10 mins</td>
-      </tr>
-      <tr style=$str:activity$>
-         <td>Play with EC2</td>
-         <td>Activity</td>
-         <td>15 mins</td>
-      </tr>
-      <tr style=$str:rest$>
-         <td colspan="2">Discussion</td>
-         <td>10 mins</td>
-      </tr>
-    </table>
-  >>
-};
-{
-  styles=[];
-  content= <:html<
     <h3>Prior Knowledge</h3>
     <ul>
-      <li>Basic OCaml</li>
+      <li>Basic programming, but not necessarily OCaml</li>
       <li>Day-to-day UNIX operation, preferably Linux or MacOS X.</li>
       <li>Version control (git)</li>
     </ul>
-    <p><b>Does everyone have the VirtualBox VM installed?</b></p><br />
+    <p>Local VM available: <tt>ssh -Y cufpXX@tutorial{1,2,3}.icfp</tt> (<i>pass:</i> cufp2012)</p>
     <h3>Software Required</h3>
     <ul>
-     <li>A 64-bit UNIX (Linux or MacOS X)</li>
+     <li>A UNIX-like OS (Linux or MacOS X)</li>
      <li>OCaml 3.12.1 or OCaml 4.00.0.</li>
     </ul>
   >>
@@ -168,6 +88,7 @@ $str:dl$ cat > ~/.ocamlinit
 #use "topfind"
 #thread
 #require "lwt";;
+#require "core.top";;
 #require "async";;
 ^D
 $str:dl$ rlwrap ocaml -I $OCAML_TOPLEVEL_PATH
